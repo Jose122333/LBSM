594d4882fc6f468e624d497aa406ead455b937f2
Jose122333 <josevela9458@gmail.com>
2016-09-26 16:05:23 +0200

    PRUEBA

--- main.c
+++ main.c
@@ -17,26 +17,25 @@
 void wait(unsigned int cycles);
 
 int main(void) {
-    
+
     unsigned int i;
 
     LED0_TRIS = OUTPUT;
-    //PRUEBA
     
     LED0 = OFF;
-    
+
     //We light up the first led
     while(1){
         LED0 = OFF;
         for(i = 0;i<1234;i++) wait(1000);
         LED0 = ON;
         for(i = 0;i<1234;i++) wait(1000);
-    }            
+    }
     return 0;
 }
 
 void wait(unsigned int cycles){
 
     unsigned int i;
-            for(i = 0;i<cycles;i++);  
+            for(i = 0;i<cycles;i++);
 }
